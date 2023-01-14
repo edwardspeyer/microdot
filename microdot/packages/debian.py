f5cd95f80380f858d07988b7993794da1e5211aa
EGS <edwardspeyer@users.noreply.github.com>
2023-01-14 17:15:18 +0000

    WIP Debian

---
+++ microdot/packages/debian.py
@@ -0,0 +1,45 @@
+DAILY_DEBIAN_PACKAGES = {
+    "apt-file",
+    "bash-completion",
+    "btop",
+    "ccze",
+    "curl",
+    "dc",
+    "dnsutils",
+    "dpkg-dev",
+    "exuberant-ctags",
+    "fzf",
+    "git",
+    "iproute2",
+    "iputils-ping",
+    "jq",
+    "less",
+    "mutt",
+    "ncdu",
+    "python3-pip",
+    "ripgrep",
+    "silversearcher-ag",
+    "sqlite3",
+    "strace",
+    "sudo",
+    "tcpdump",
+    "tmux",
+    "units",
+    "vim-gtk3",  # Terminal vim with lua support"
+    "w3m",
+    "wget",
+    "xz-utils",
+}
+
+CREATIVE_DEBIAN_PACKAGES = {
+    # Admin
+    "locales",
+    "man-db",
+    # Tools
+    "docker.io",
+    "ffmpeg",
+    "firefox",
+    "git-lfs",
+    "postgresql-client",
+    "ubuntu-standard",
+}
