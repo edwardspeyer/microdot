d575e5d53ad2674282a5ddeeeadd82537d5546cf
EGS <edwardspeyer@users.noreply.github.com>
2023-01-21 16:48:39 +0000

    Install useful terminfos

---
+++ microdot/terminfo.py
@@ -0,0 +1,27 @@
+import platform
+from pathlib import Path
+from shutil import copy
+from typing import Iterable
+
+
+def install(*sources: Iterable[Path]):
+    for source in sources:
+        _install_one(source)
+
+
+def _install_one(source: Path):
+    first_letter = source.name[0]
+
+    if platform.system() == "Linux":
+        sub = first_letter
+    elif platform.system() == "Darwin":
+        sub = f"{ord(first_letter):x}"
+    else:
+        raise Exception("unknown platform")
+
+    destination = Path.home() / ".terminfo" / sub / source.name
+    if destination.exists():
+        return
+    destination.parent.mkdir(parents=True, exist_ok=True)
+    print("  TERMINFO", source)
+    copy(source, destination)
