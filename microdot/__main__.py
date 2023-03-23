901174c4f5b645d3de9a1b0cf1eb00d97193f47d
EGS <edwardspeyer@users.noreply.github.com>
2023-03-23 15:11:26 +0000

    Separate hooks into their own module

--- microdot/__main__.py
+++ microdot/__main__.py
@@ -1,134 +1,13 @@
 from pathlib import Path
-from subprocess import run
 
-from . import fonts, gnupg, packages, terminfo, x11
-from .hooks import BOTTOM, TOP, install_hook
+import microdot.hooks.install as hooks
+
+from . import fonts, gnupg, packages, terminfo
 
 BASE = Path(__file__).parent.parent.resolve()
 
-install_hook(
-    path="~/.tmux.conf",
-    comment="#",
-    position=TOP,
-    text=f"""\
-    source {BASE}/tmux/tmux.conf
-    """,
-)
-
-terminfo.install(*BASE.glob("tmux/terminfo/*"))
-
-install_hook(
-    path="~/.vimrc",
-    comment='"',
-    position=TOP,
-    text=f"""\
-    source {BASE}/vim/vimrc
-    """,
-)
-
-run(
-    """
-    mkdir -p ~/.vim/autoload/
-    cp vim/autoload/* ~/.vim/autoload
-    tar -C ~/.vim/ -xf vim/plugged.tar.gz
-    """,
-    shell=True,
-    check=True,
-)
-
-install_hook(
-    path="~/.bashrc",
-    comment="#",
-    position=TOP,
-    text=f"""\
-    source {BASE}/bash/bashrc
-    """,
-)
-
-install_hook(
-    path="~/.gitconfig",
-    comment="#",
-    position=TOP,
-    text=f"""\
-    [include]
-    path = {BASE}/git/config
-
-    [core]
-    excludesfile = {BASE}/git/ignore
-    """,
-)
-
-install_hook(
-    path="~/.ssh/config",
-    comment="#",
-    position=BOTTOM,
-    text=f"""\
-    Host *
-    Include {BASE}/ssh/config
-    """,
-)
-
-install_hook(
-    path="~/.config/fish/conf.d/microdot.fish",
-    comment="#",
-    position=TOP,
-    text=f"""\
-    for file in {BASE}/fish/*.fish
-        source $file
-    end
-    """,
-)
-
-install_hook(
-    path="~/.config/apt.conf",
-    comment="//",
-    position=TOP,
-    text=f"""\
-    // Requires APT_CONFIG to also be set in the environment
-    #include "{BASE}/apt/apt.conf";
-    """,
-)
-
-install_hook(
-    path="~/.config/kitty/kitty.conf",
-    comment="#",
-    position=TOP,
-    text=f"""\
-    include {BASE}/kitty/kitty.conf
-    """,
-)
-
-install_hook(
-    path="~/.config/i3/config",
-    comment="#",
-    position=TOP,
-    text=f"""\
-    include {BASE}/i3/config
-    """,
-)
-
-install_hook(
-    path="~/.xsession",
-    comment="#",
-    mode=0o744,
-    position=BOTTOM,
-    text=f"""\
-    # Hand control to microdot's xsession
-    exec {BASE}/X11/xsession
-    """,
-)
-
-install_hook(
-    path="~/.muttrc",
-    comment="#",
-    position=BOTTOM,
-    text=f"""\
-    source {BASE}/mutt/muttrc
-    """,
-)
-
+hooks.install()
 terminfo.install(*BASE.glob("kitty/terminfo/*"))
-
 fonts.install()
 gnupg.install()
 packages.debian.install()
---
+++ microdot/hooks/install.py
@@ -0,0 +1,127 @@
+from pathlib import Path
+from subprocess import run
+
+from microdot.hooks import BOTTOM, TOP, install_hook
+
+BASE = Path(__file__).parent.parent.parent.resolve()
+
+
+def install():
+    install_hook(
+        path="~/.tmux.conf",
+        comment="#",
+        position=TOP,
+        text=f"""\
+        source {BASE}/tmux/tmux.conf
+        """,
+    )
+
+    install_hook(
+        path="~/.vimrc",
+        comment='"',
+        position=TOP,
+        text=f"""\
+        source {BASE}/vim/vimrc
+        """,
+    )
+
+    run(
+        """
+        mkdir -p ~/.vim/autoload/
+        cp vim/autoload/* ~/.vim/autoload
+        tar -C ~/.vim/ -xf vim/plugged.tar.gz
+        """,
+        shell=True,
+        check=True,
+    )
+
+    install_hook(
+        path="~/.bashrc",
+        comment="#",
+        position=TOP,
+        text=f"""\
+        source {BASE}/bash/bashrc
+        """,
+    )
+
+    install_hook(
+        path="~/.gitconfig",
+        comment="#",
+        position=TOP,
+        text=f"""\
+        [include]
+        path = {BASE}/git/config
+
+        [core]
+        excludesfile = {BASE}/git/ignore
+        """,
+    )
+
+    install_hook(
+        path="~/.ssh/config",
+        comment="#",
+        position=BOTTOM,
+        text=f"""\
+        Host *
+        Include {BASE}/ssh/config
+        """,
+    )
+
+    install_hook(
+        path="~/.config/fish/conf.d/microdot.fish",
+        comment="#",
+        position=TOP,
+        text=f"""\
+        for file in {BASE}/fish/*.fish
+            source $file
+        end
+        """,
+    )
+
+    install_hook(
+        path="~/.config/apt.conf",
+        comment="//",
+        position=TOP,
+        text=f"""\
+        // Requires APT_CONFIG to also be set in the environment
+        #include "{BASE}/apt/apt.conf";
+        """,
+    )
+
+    install_hook(
+        path="~/.config/kitty/kitty.conf",
+        comment="#",
+        position=TOP,
+        text=f"""\
+        include {BASE}/kitty/kitty.conf
+        """,
+    )
+
+    install_hook(
+        path="~/.config/i3/config",
+        comment="#",
+        position=TOP,
+        text=f"""\
+        include {BASE}/i3/config
+        """,
+    )
+
+    install_hook(
+        path="~/.xsession",
+        comment="#",
+        mode=0o744,
+        position=BOTTOM,
+        text=f"""\
+        # Hand control to microdot's xsession
+        exec {BASE}/X11/xsession
+        """,
+    )
+
+    install_hook(
+        path="~/.muttrc",
+        comment="#",
+        position=BOTTOM,
+        text=f"""\
+        source {BASE}/mutt/muttrc
+        """,
+    )
