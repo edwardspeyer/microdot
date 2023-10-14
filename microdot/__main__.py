ffe1c7056eb577042a223ff94b73d939a91d9ef1
EGS <edwardspeyer@users.noreply.github.com>
2023-10-14 13:04:36 +0100

    Disable everything except actual dotfiles

--- microdot/__main__.py
+++ microdot/__main__.py
@@ -2,15 +2,9 @@ from pathlib import Path
 
 import microdot.hooks.install as hooks
 
-from . import fonts, gnupg, packages, terminfo
+from . import terminfo
 
 BASE = Path(__file__).parent.parent.resolve()
 
 hooks.install()
 terminfo.install(*BASE.glob("kitty/terminfo/*"))
-fonts.install()
-gnupg.install()
-packages.debian.install()
-packages.pip.install()
-packages.source.install()
-# x11.install()
