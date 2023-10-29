2b02c8c4649b092c60c7e19454bbaabefb6b0b06
EGS <edwardspeyer@users.noreply.github.com>
2023-10-29 12:44:31 +0000

    Thunderbird look & feel

--- microdot/__main__.py
+++ microdot/__main__.py
@@ -1,9 +1,10 @@
 from pathlib import Path
 
 import microdot.hooks.install as hooks
-from microdot import terminfo
+from microdot import terminfo, thunderbird
 
 BASE = Path(__file__).parent.parent.resolve()
 
 hooks.install()
 terminfo.install(*BASE.glob("kitty/terminfo/*"))
+thunderbird.install()
