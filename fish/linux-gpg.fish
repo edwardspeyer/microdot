2fce3d56184a3bd0cf306cdc413bd39c5181566d
EGS <edwardspeyer@users.noreply.github.com>
2023-12-23 16:44:28 +0000

    GnuPG directory fixes

--- fish/linux-gpg.fish
+++ fish/linux-gpg.fish
@@ -1,3 +1,15 @@
+if not command -v gpg-connect-agent >/dev/null
+        return
+end
+
+# Ensure .gnupg exists.
+#
+# Weirdly (once) it was there but it wasn't a file.
+if not test -d ~/.gnupg
+        rm -f ~/.gnupg
+end
+mkdir -p ~/.gnupg
+
 if status --is-interactive; and test (uname) = Linux
         set -x SSH_AUTH_SOCK /run/user/1000/gnupg/S.gpg-agent.ssh
         gpg-connect-agent updatestartuptty /bye >/dev/null
