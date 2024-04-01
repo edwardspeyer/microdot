111ba08017642274c030503da814da8387031a77
EGS <edwardspeyer@users.noreply.github.com>
2024-04-01 17:22:45 +0100

    Auto-install SFMono

--- microdot/fonts.py
+++ microdot/fonts.py
@@ -1,11 +1,16 @@
 import platform
+import re
 from pathlib import Path
 from shutil import copy
+from subprocess import check_output, run
+from tempfile import TemporaryDirectory
 
 
 def install():
     if platform.system() == "Darwin":
         install_macosx()
+    elif platform.system() == "Linux":
+        install_linux()
 
 
 def install_macosx():
@@ -20,3 +25,37 @@ def install_macosx():
             continue
         print("  FONT", source.name)
         copy(source, target)
+
+
+def install_linux():
+    if re.search(r"SF.*Mono", check_output("fc-list", text=True)):
+        print("  OK  ", "SF Mono installation")
+        return
+
+    with TemporaryDirectory() as tmp:
+        run(
+            """
+            set -ex
+            url="https://devimages-cdn.apple.com/design/resources/download/SF-Mono.dmg"
+            sudo apt install --yes 7zip cpio
+            wget -O dmg $url
+            ls -l dmg
+            file dmg
+            7zz x dmg
+            7zz x "SFMonoFonts/SF Mono Fonts.pkg"
+            cpio -idv -F "Payload~"
+            find .
+            """,
+            shell=True,
+            cwd=tmp,
+            check=True,
+        )
+        target = Path.home() / ".local" / "share" / "fonts"
+        target.mkdir(exist_ok=True, parents=True)
+        for source in Path(tmp).glob("**/*.otf"):
+            print("  FONT", source.name)
+            copy(source, target)
+
+
+if __name__ == "__main__":
+    install()
