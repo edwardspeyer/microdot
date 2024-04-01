035dd72947e5221e9b6fcdfc5281ee92564a742f
EGS <edwardspeyer@users.noreply.github.com>
2024-04-01 17:56:47 +0100

    Separate source-installs into modules

--- microdot/packages/source/__init__.py
+++ microdot/packages/source/__init__.py
@@ -1,72 +1,31 @@
 import os
-import platform
 from pathlib import Path
 from shutil import copytree
-from subprocess import run
+from subprocess import check_output, run
 from tempfile import TemporaryDirectory
 
-FISH_VERSION = "3.6.0"
-I3_VERSION = "4.22"
-TMUX_VERSION = "3.3a"
-DELTA_VERSION = "0.15.1"
 
-
-def install():
-    if not is_linux():
-        return
-    install_delta()
-    install_fish()
-    install_i3()
-    install_tmux()
-
-
-def install_delta():
-    if DELTA_VERSION in shell_output("delta --version"):
-        return
-    script_install("delta", DELTA_VERSION)
-
-
-def install_fish():
-    if FISH_VERSION in shell_output("fish --version"):
-        return
-    script_install("fish", FISH_VERSION)
-
-
-def install_i3():
-    if I3_VERSION in shell_output("i3 --version"):
-        return
-    script_install("i3", I3_VERSION)
-
-
-def install_tmux():
-    if TMUX_VERSION in shell_output("tmux -V"):
-        return
-    script_install("tmux", TMUX_VERSION)
-
-
-def shell_output(script):
-    return run(
-        script,
-        capture_output=True,
-        check=False,
-        shell=True,
-        text=True,
-    ).stdout.strip()
-
-
-def script_install(name, *args):
+def install_from_script(script, *args):
+    """Run the script and assume everything everything in $tmp/out goes
+    into ~/.local."""
     with TemporaryDirectory() as temporary_directory:
         tmp = Path(temporary_directory)
-        base = Path(__file__).parent
-        script = base / f"{name}.sh"
         env = dict(os.environ)
         env["DEBIAN_FRONTEND"] = "noninteractive"
         env["PATH"] = f"/usr/lib/ccache:{env['PATH']}"
-        run(["sh", script, *args], check=True, cwd=tmp, env=env)
+        run(["sh", "-c", script, "--", *args], check=True, cwd=tmp, env=env)
         out = tmp / "out"
         local = Path.home() / ".local"
         copytree(out, local, dirs_exist_ok=True)
 
 
-def is_linux():
-    return platform.system() == "Linux"
+def get_version(command: str, option: str):
+    output = check_output([command, option], text=True)
+    for token in output.split():
+        if "." in token:
+            return parse_version(token)
+    raise
+
+
+def parse_version(text: str):
+    return [int(v) if v.isnumeric() else v for v in text.split(".")]
---
+++ microdot/packages/source/tmux.py
@@ -0,0 +1,30 @@
+import platform
+from shutil import which
+
+from microdot.packages.source import get_version, install_from_script, parse_version
+
+VERSION = "3.3a"
+
+SCRIPT = r"""
+set -ex
+sudo apt install --yes --no-install-recommends \
+  autoconf automake bison build-essential ca-certificates ccache git \
+  libevent-dev libncurses-dev pkg-config
+git clone --depth 1 --branch "$1" https://github.com/tmux/tmux.git .
+sh autogen.sh
+./configure --enable-static --prefix ./out
+make install
+"""
+
+
+def install():
+    if which("tmux"):
+        if get_version("tmux", "-V") >= parse_version(VERSION):
+            return
+
+    assert platform.system() == "Linux"
+    install_from_script(SCRIPT, VERSION)
+
+
+if __name__ == "__main__":
+    install()
