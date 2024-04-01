b30914d5d5090c5764239ef03a1f85ca45ebba1f
EGS <edwardspeyer@users.noreply.github.com>
2024-04-01 18:27:34 +0100

    Make microdot.hooks just another script

--- microdot/hooks.py
+++ microdot/hooks.py
@@ -1,9 +1,56 @@
+import re
+import textwrap
+from enum import Enum
 from pathlib import Path
 from subprocess import run
 
-from microdot.hooks import Position, install_hook
+BASE = Path(__file__).parent.parent.resolve()
 
-BASE = Path(__file__).parent.parent.parent.resolve()
+
+class Position(Enum):
+    TOP = "TOP"
+    BOTTOM = "BOTTOM"
+
+
+def log(action, path):
+    print(f"  {action:6s}  {path}", flush=True)
+
+
+def install_hook(
+    path: Path,
+    comment: str,
+    position: Position,
+    text: str,
+    mode: int = 0o644,
+):
+    inner = textwrap.dedent(text).rstrip("\n") + "\n"
+    begin = f"{comment} -----BEGIN MICRODOT-----\n"
+    end = f"{comment} -----END MICRODOT-----\n"
+    outer = begin + inner + end
+    pattern = re.compile(f"{begin}.+?\n{end}", re.DOTALL)
+
+    if not path.exists():
+        log("NEW", path)
+        path.parent.mkdir(exist_ok=True, parents=True, mode=0o700)
+        path.touch(mode=mode)
+        path.write_text(outer)
+        return
+
+    old = path.read_text()
+    new = re.sub(pattern, outer, old)
+
+    if re.search(pattern, old):
+        if old == new:
+            log("SAME", path)
+        else:
+            log("UPDATE", path)
+            path.write_text(new)
+    else:
+        log("EDIT", path)
+        if position == Position.TOP:
+            path.write_text(outer + "\n" + old)
+        elif position == Position.BOTTOM:
+            path.write_text(old + "\n" + outer)
 
 
 def install():
@@ -136,3 +183,7 @@ def install():
         source {BASE}/mutt/muttrc
         """,
     )
+
+
+if __name__ == "__main__":
+    install()
