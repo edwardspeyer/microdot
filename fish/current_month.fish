bca8120c557e45d03fc49c488e156e6edbabcd32
EGS <edwardspeyer@users.noreply.github.com>
2024-07-23 09:03:28 +0100

    Fish `Current` helper

--- fish/current_month.fish
+++ fish/current_month.fish
@@ -1,6 +1,7 @@
-function create-current-documents-month
+function current-projects-month
         set month (date +%Y-%m)
         mkdir -p ~/Projects/Months/$month
         ln -nsf Months/$month ~/Projects/Current
         cd ~/Projects/Current
+        echo Months/$month
 end
