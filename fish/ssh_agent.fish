429cb581b71832552f7d194aa274d330559441f7
EGS <edwardspeyer@users.noreply.github.com>
2025-05-07 11:47:29 +0100

    Better ssh-agent logic

--- fish/ssh_agent.fish
+++ fish/ssh_agent.fish
@@ -1,27 +1,30 @@
-# Ensure we are connected to an agent, if one exists.
+function ssh_agent_start
+    # Ensure we are connected to an agent, if one exists.
 
-function ssh_test_socket
-    env SSH_AUTH_SOCK=$argv[1] ssh-add -l >/dev/null 2>&1
-    switch $status
-        case 0 1
-            return 0
-        case '*'
-            echo "status: $status"
+    function ssh_agent_test_socket
+        env SSH_AUTH_SOCK=$argv[1] ssh-add -l >/dev/null 2>&1
+        switch $status
+            case 0 1
+                return 0
+            case '*'
+                echo "status: $status"
+        end
+        return 1
     end
-    return 1
-end
 
-
-function ssh_connect_to_existing_agent
+    # Look for existing agents...
     for socket in /tmp/ssh-*/agent.*
-        if ssh_test_socket $socket
+        if ssh_agent_test_socket $socket
             set -gx SSH_AUTH_SOCK $socket
             return
         end
     end
+
+    # ...or start a new one
+    eval (ssh-agent -c) >/dev/null
 end
 
 
 if status --is-interactive; and not set -q $SSH_AUTH_SOCK
-    ssh_connect_to_existing_agent
+    ssh_agent_start
 end
