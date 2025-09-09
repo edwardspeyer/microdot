3309e6131b7aa9c721fbc9b299cd6f509a7b15ae
EGS <edwardspeyer@users.noreply.github.com>
2025-09-09 14:46:13 +0100

    Move thunderbird config to top level

---
+++ thunderbird/configure.py
@@ -0,0 +1,27 @@
+from configparser import ConfigParser
+from pathlib import Path
+from typing import Iterator
+
+from microdot import install_hook, register
+
+
+def profile_directories() -> Iterator[Path]:
+    thunderbird_config_root = Path.home() / ".thunderbird"
+    c = ConfigParser()
+    c.read(thunderbird_config_root / "profiles.ini")
+    for section in c.sections():
+        if "Profile" not in section:
+            continue
+        yield thunderbird_config_root / c[section]["Path"]
+
+
+@register
+def install():
+    base = Path(__file__).parent.parent
+    config_path = base / "thunderbird" / "user.js"
+    if not config_path.exists():
+        return
+    config = config_path.read_text()
+    for dir in profile_directories():
+        dest = dir / "user.js"
+        install_hook(dest, config, comment="// ")
