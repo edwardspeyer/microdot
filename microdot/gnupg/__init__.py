662cbb80b8532000a8073b5641b5798638226fd0
EGS <edwardspeyer@users.noreply.github.com>
2025-09-10 17:40:49 +0100

    Quieter gpg output

--- microdot/gnupg/__init__.py
+++ microdot/gnupg/__init__.py
@@ -8,11 +8,12 @@ from microdot import cwd
 def install():
     dot_gnupg = Path.home() / ".gnupg"
     dot_gnupg.mkdir(exist_ok=True)
+    dot_gnupg.chmod(0o700)
 
     # Install config
     copy(cwd() / "gpg-agent.conf", dot_gnupg)
 
     # Import key stubs
-    script = 'gpg --import "$1"/stubs/* && pkill gpg-agent || true'
+    script = 'gpg --quiet --import "$1"/stubs/* && pkill gpg-agent || true'
     command = ["sh", "-c", script, "--", str(cwd())]
     run(command, check=True)
